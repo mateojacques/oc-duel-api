fe2c5721d2457a151fda91d2dc2fa03321109aa2
Mateo Jacques <mateojacquesweb@gmail.com>
2025-03-03 09:24:02 -0300

    feat: add multer configuration

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ DB_HOST={Database host}
 DB_USER={Database user name}
 DB_PASSWORD={Database user password}
 DB_PORT={Database port}
+AWS_ACCESS_KEY_ID={S3 Access Key}
+AWS_SECRET_ACCESS_KEY={S3 Secret Access Key}
+AWS_REGION={S3 region}
+S3_BUCKET_NAME={S3 Bucket name}
---
+++ src/config/multer.ts
@@ -0,0 +1,31 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.S3_BUCKET_NAME!,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      const uniqueFileName = `${Date.now()}-${file.originalname}`;
+      cb(null, uniqueFileName);
+    },
+  }),
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
+});
+
+export default upload;
